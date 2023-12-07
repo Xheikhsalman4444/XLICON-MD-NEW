2f6fef6d9f463deec89f24e79127e346eb26d04f
ABRAHAM DWAMENA <120536940+abrahamdw882@users.noreply.github.com>
2023-12-07 13:53:33 +0000

    Update owner-banchat.js

--- plugins/owner-banchat.js
+++ plugins/owner-banchat.js
@@ -3,7 +3,7 @@
 let handler = async (m, { conn, isOwner, isAdmin, isROwner }) => {
     if (!(isAdmin || isOwner)) return dfail('admin', m, conn)
     global.db.data.chats[m.chat].isBanned = true
-    m.reply('✅ The bot was deactivated in this group')
+    m.reply('The bot has been deactivated in this gc 😔')
 }
 handler.help = ['banchat']
 handler.tags = ['owner']
