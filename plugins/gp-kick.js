afa0d555b49ef5142bf59f12166e611d9a1ef7c9
ABRAHAM DWAMENA <120536940+abrahamdw882@users.noreply.github.com>
2023-12-07 13:54:59 +0000

    Update gp-kick.js

--- plugins/gp-kick.js
+++ plugins/gp-kick.js
@@ -7,7 +7,7 @@ if (!m.mentionedJid[0] && !m.quoted) return m.reply(kickte, m.chat, { mentions: 
 let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
 let owr = m.chat.split`-`[0]
 await conn.groupParticipantsUpdate(m.chat, [user], 'remove')
-m.reply(`✅ User kicked`) 
+m.reply(`🥲 User has been kicked out`) 
 
 }
 
