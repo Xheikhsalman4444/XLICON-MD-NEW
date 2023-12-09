566ea6acae921c1cfb45566335ba9099d0f90b9e
ABRAHAM DWAMENA <120536940+abrahamdw882@users.noreply.github.com>
2023-12-09 23:53:51 +0000

    Update Wall-couplepp.js

--- plugins/Wall-couplepp.js
+++ plugins/Wall-couplepp.js
@@ -5,9 +5,9 @@ let handler = async (m, { conn }) => {
   let cita = data[Math.floor(Math.random() * data.length)]
   
   let cowi = await(await fetch(cita.cowo)).buffer()
-  await conn.sendFile(m.chat, cowi, '', '♂️', m)
+  await conn.sendFile(m.chat, cowi, '', '♂️ Boy🧑‍✈️', m)
   let ciwi = await(await fetch(cita.cewe)).buffer()
-  await conn.sendFile(m.chat, ciwi, '', '♀️', m)
+  await conn.sendFile(m.chat, ciwi, '', '♀️ Girl👰', m)
 }
 handler.help = ['ppcouple', 'ppcp']
 handler.tags = ['img']
