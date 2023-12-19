ab8b13e6c8e13798b6f00e5990c3881212da5d57
ABRAHAM DWAMENA <120536940+abrahamdw882@users.noreply.github.com>
2023-12-19 20:53:30 +0000

    Create cat.js

---
+++ plugins/cat.js
@@ -0,0 +1,23 @@
+import { exec } from 'child_process'
+import speed from 'performance-now'
+
+const {
+    tlang,
+    prefix, 
+    Config ,
+    sleep,
+    getBuffer, 
+     } = require('../lib')
+
+  async(m)=>{
+  try{
+    await m.send('https://cataas.com/cat',{caption:"*meyaoooooooooooooon!*"},"img",m)
+  }catch(e){ m.error(`${e}\n\nCommand: cat`,e,false)}
+  })
+  
+
+handler.help = ['cat']
+handler.tags = ['logo']
+handler.command = ['cat'] 
+
+export default handler￼Enter
