a224be2c7f2f976bf2697c543519a57004260083
ABRAHAM DWAMENA <120536940+abrahamdw882@users.noreply.github.com>
2023-12-19 11:21:25 +0000

    Create main-hack.js

---
+++ plugins/main-hack.js
@@ -0,0 +1,50 @@
+import { exec } from 'child_process'
+import speed from 'performance-now'
+
+async(m) => {    
+await m.send("Injecting Malware")   
+await sleep(2000)    
+await m.send(" █ 10%")    
+await sleep(1000)    
+await m.send(" █ █ 20%")    
+await sleep(1000)    
+await m.send(" █ █ █ 30%")    
+await sleep(1000)    
+await m.send(" █ █ █ █ 40%")    
+await sleep(1000)    
+await m.send(" █ █ █ █ █ 50%")    
+await sleep(1000)    
+await m.send(" █ █ █ █ █ █ 60%")    
+await sleep(1000)    
+await m.send(" █ █ █ █ █ █ █ 70%")    
+await sleep(1000)    
+await m.send(" █ █ █ █ █ █ █ █ 80%")    
+await sleep(1000)    
+await m.send(" █ █ █ █ █ █ █ █ █ 90%")    
+await sleep(1000)    
+await m.send(" █ █ █ █ █ █ █ █ █ █ 100%")    
+await sleep(1000)    
+await m.send("System hyjacking on process.. \n Conecting to Server error to find 404 ")    
+await sleep(1000)    
+await m.send("Device successfully connected... \n Riciving data...")    
+await sleep(1000)    
+await m.send("Data hyjacked from divice 100% completed \n killing all evidence killing all malwares...")
+await sleep(1000)    
+await m.send(" HACKING COMPLETED ")    
+await sleep(2000)    
+await m.send(" SENDING LOG DOCUMENTS...")    
+await sleep(1000)
+await m.send(" SUCCESSFULLY SENT DATA AND Connection disconnected")    
+await sleep(1000)
+
+    return await m.send('BACKLOGS CLEARED');
+
+}
+
+)
+
+handler.help = ['hack']
+handler.tags = ['main']
+handler.command = ['hack', 'xhack'] 
+
+export default handler
