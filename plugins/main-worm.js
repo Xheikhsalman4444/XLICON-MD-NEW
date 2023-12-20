0d78aacf8bff8ec35c7071714b0f2006fdeded96
ABRAHAM DWAMENA <120536940+abrahamdw882@users.noreply.github.com>
2023-12-20 14:55:01 +0000

    Update main-worm.js

--- plugins/main-worm.js
+++ plugins/main-worm.js
@@ -1,12 +1,13 @@
 import fetch from 'node-fetch';
 
-let handler = async (m, { text, conn, usedPrefix, command }) => {
+let handler = async WormGPT({ text, conn, usedPrefix, command }) {
+
 const axios = require('axios')
 let data = await axios.get(`https://flowgpt.com/p/wormgpt-v30/gpt3?prompt=Hello`);
 
 return data
 
-m();
+WormGPT();
 }
 handler.help = ['worm']
 handler.tags = ['AI']
