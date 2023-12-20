72fc6a3e9a848a621ab827b731d5c897f094932a
ABRAHAM DWAMENA <120536940+abrahamdw882@users.noreply.github.com>
2023-12-20 21:55:09 +0000

    Update bard1.js

--- plugins/bard1.js
+++ plugins/bard1.js
@@ -7,7 +7,7 @@ var handler = async (m, {
  command
  }) => {
 if (!text) throw `Masukkan pertanyaan!\n\n*Contoh:* kenapa wanita bisa hamil? `
-await conn.sendMessage(m.chat, { contextInfo: { externalAdReply: { title: 'Tunggu sebentar kak',  thumbnailUrl: `https://telegra.ph/file/1ccd736159d3566c3895e.jpg` }}}, { quoted: m });
+await conn.sendMessage(m.chat, { quoted: m });
   var js = await fetch(API('lann', '/api/search/bard-ai', { apikey: beta, text: `${text}` }))
 var json = await js.json()
 try {
@@ -25,7 +25,7 @@ renderLargerThumbnail: true
 } catch (err) {
 conn.sendMessage(m.chat, { sticker: errorstc, contextInfo: { externalAdReply: { title: 'eror 👻',  thumbnailUrl: rawr2 }}}, { quoted: m });
 }}
-handler.command = handler.help = ['bard','gbard','goobard'];
+handler.command = handler.help = ['bardi','gbard','goobard'];
 handler.tags = ['info'];
 handler.register = false
 handler.limit = 3
