9eea162fa7e26bb295c467701a4aba4361cf587e
ABRAHAM DWAMENA <120536940+abrahamdw882@users.noreply.github.com>
2023-12-21 20:31:17 +0000

    Update and rename V to main-cat.js

---
+++ plugins/main-cat.js
@@ -0,0 +1,24 @@
+import { exec } from 'child_process';
+import speed from 'performance-now';
+import cheerio from 'cheerio';
+import fetch from 'node-fetch';
+
+let handler = async (m, {
+  conn,
+  args,
+  usedPrefix,
+  text,
+  command
+}) => {
+  try {
+    await m.reply('https://cataas.com/cat', { caption: "*meyaoooooooooooooon!*" }, "img", m);
+  } catch (e) {
+    m.error(`${e}\n\nCommand: cat`, e, false);
+  }
+};
+
+handler.help = ['cat'];
+handler.tags = ['logo'];
+handler.command = ['cat'];
+
+export default handler;
