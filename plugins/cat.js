c1f3a5245d75efff70ff4bd7c185949f1d258e48
ABRAHAM DWAMENA <120536940+abrahamdw882@users.noreply.github.com>
2023-12-21 11:35:38 +0000

    Update cat.js

--- plugins/cat.js
+++ plugins/cat.js
@@ -1,23 +1,24 @@
-import { exec } from 'child_process'
-import speed from 'performance-now'
+import { exec } from 'child_process';
+import speed from 'performance-now';
 import cheerio from 'cheerio';
 import fetch from 'node-fetch';
 
-    let handler = async (m, {
-    conn,
-    args,
-    usedPrefix,
-    text,
-    command
+let handler = async (m, {
+  conn,
+  args,
+  usedPrefix,
+  text,
+  command
 }) => {
-  try{
-    await m.reply('https://cataas.com/cat',{caption:"*meyaoooooooooooooon!*"},"img",m)
-  }catch(e){ m.error(`${e}\n\nCommand: cat`,e,false)}
-  })
-  
+  try {
+    await m.reply('https://cataas.com/cat', { caption: "*meyaoooooooooooooon!*" }, "img", m);
+  } catch (e) {
+    m.error(`${e}\n\nCommand: cat`, e, false);
+  }
+};
 
-handler.help = ['cat']
-handler.tags = ['logo']
-handler.command = ['cat'] 
+handler.help = ['cat'];
+handler.tags = ['logo'];
+handler.command = ['cat'];
 
-export default handler
+export default handler;
